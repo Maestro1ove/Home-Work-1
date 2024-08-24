d5729a1114e6365aa5df67bf3e5816deb886c305
Daniel <danielmanchanda@gmail.com>
2024-08-25 01:08:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter.scrolledtext import example
+
+print ('Hello teacher') # Буду благодарен комментариям моей работы!
+# 1st program
+print (9 ** 0.5 * 5)
+# 2st program
+print ( 9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print (2 * 2 + 2 == 2 * (2 + 2)) # Не совсем понял, нужно ли мне было считать 2 строчки сверху, по сути решение тут на одной строке изображено
+# 4th program
+num_str = '123.456'
+num = float(num_str)
+print ( num * 10)
+print ('Thank you , Good Luck')
+
+
+
+
